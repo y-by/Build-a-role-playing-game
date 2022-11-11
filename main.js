05a7e9978f53bc7ecb70f60805e309ff46ecefe6
janay <janayby@gmail.com>
2022-11-11 09:29:58 +0200

    change forloop to new Array of rand num .fill,.map

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function getDiceRollArray(diceCount) {
-  const newDiceRolls = []
-  for(let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1)
-  }
-  return newDiceRolls
+  return new Array(diceCount).fill(0).map(function(){
+    return Math.floor(Math.random() * 6) + 1
+  })
 }
 
 function getDiceHtml(diceCount) {
