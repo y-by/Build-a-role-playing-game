7627063c3f45e475528b1f395c0f56b655c232f3
janay <janayby@gmail.com>
2022-11-13 08:52:24 +0200

    add constractor function with method toRender html

--- main.js
+++ main.js
@@ -26,20 +26,32 @@ const monster = {
   diceCount: 1
 }
 
-function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceCount } = data;
-  const diceHtml = getDiceHtml(diceCount)
+function Character(data) {
+  this.elementId = data.elementId;
+  this.name = data.name;
+  this.avatar = data.avatar;
+  this.health = data.health;
+  this.diceCount = data.diceCount;
 
-  document.getElementById(elementId).innerHTML =
-      `<div class="character-card">
+  this.getCharacterHtml = function () {
+      const { elementId, name, avatar, health, diceCount } = this;
+      const diceHtml = getDiceHtml(diceCount)
+
+      document.getElementById(elementId).innerHTML =
+          `<div class="character-card">
           <h4 class="name"> ${name} </h4>
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
-          <div class="dice-container">
+          <div class="dice-container">    
               ${diceHtml}
           </div>
       </div>`
+
+  }
+
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+const wizard = new Character(hero)
+const orc = new Character(monster)
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
