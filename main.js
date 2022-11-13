b3ef37242ee26781b471e1e4983bf14b450fcc64
janay <janayby@gmail.com>
2022-11-13 09:50:40 +0200

    bind this to data with Object.assign

--- main.js
+++ main.js
@@ -27,11 +27,7 @@ const monster = {
 }
 
 function Character(data) {
-  this.elementId = data.elementId;
-  this.name = data.name;
-  this.avatar = data.avatar;
-  this.health = data.health;
-  this.diceCount = data.diceCount;
+  Object.assign(this, data)
 
   this.getCharacterHtml = function () {
       const { elementId, name, avatar, health, diceCount } = this;
