26fb54d9d722f8a1b39efb5dc5c3e3723c40ed59
janay <janayby@gmail.com>
2022-11-18 16:55:01 +0200

    wrap characters in an object

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ function getDiceRollArray(diceCount) {
     return Math.floor(Math.random() * 6) + 1
   })
 }
+const characterData = {
+  hero: {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceCount: 3
+  },
 
-const hero = {
-  elementId: "hero",
-  name: "Wizard",
-  avatar: "images/wizard.png",
-  health: 60,
-  diceCount: 3
+  monster: {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceCount: 1
+  }
 }
-
-const monster = {
-  elementId: "monster",
-  name: "Orc",
-  avatar: "images/orc.png",
-  health: 10,
-  diceCount: 1
-}
-
 function Character(data) {
   Object.assign(this, data)
 
@@ -46,13 +46,13 @@ function Character(data) {
 
 }
 
-const wizard = new Character(hero)
-const orc = new Character(monster)
 
 function render(){
   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
 }
 
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 render()
 
