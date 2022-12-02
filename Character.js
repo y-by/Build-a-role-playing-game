1a6b4d78003048402be4870c9e5e67e6591306f6
janay <janayby@gmail.com>
2022-12-02 07:58:05 +0200

    change dice Array to dice Html

--- Character.js
+++ Character.js
@@ -2,13 +2,14 @@ import {getDiceRollArray, getDicePlaceholderHtml, getPercentage} from "./utils.j
 
 function Character(data) {
   Object.assign(this, data)
-  this.diceArray = getDicePlaceholderHtml(this.diceCount)
+  
+  this.diceHtml = getDicePlaceholderHtml(this.diceCount)
 
   this.maxHealth = this.health
 
   this.getDiceHtml = function() {
     this.currentDiceScore = getDiceRollArray(this.diceCount)
-    this.diceArray = this.currentDiceScore.map((num) => 
+    this.diceHtml = this.currentDiceScore.map((num) => 
         `<div class="dice">${num}</div>`).join("")
   }
 
@@ -37,8 +38,7 @@ function Character(data) {
   }
 
   this.getCharacterHtml = function () {
-      const { elementId, name, avatar, health, diceCount, diceArray } = this;
-      const diceHtml = this.getDiceHtml(diceCount)
+      const { elementId, name, avatar, health, diceCount, diceHtml } = this;
       const healthBar = this.getHealthBarHtml()
           return `<div class="character-card">
           <h4 class="name"> ${name} </h4>
@@ -46,7 +46,7 @@ function Character(data) {
           <div class="health">health: <b> ${health} </b></div>
           ${healthBar}
           <div class="dice-container">    
-              ${diceArray}
+              ${diceHtml}
           </div>
       </div>`
 
